7a18baee8d1994b9f950245074ae02798649a534
Roberta2017 <99208036+Roberta2017@users.noreply.github.com>
2022-04-05 09:35:36 +0200

    Add files via upload

---
+++ 03-Variabili-e-readonly/exercise-3.js
@@ -0,0 +1,12 @@
+const students = ['Paul', 'George', 'Lucas'];
+
+function addStudent(student) {
+  students.push(student);
+  console.log(students);
+
+}
+
+addStudent('Marco');
+
+
+addStudent('antonello');
