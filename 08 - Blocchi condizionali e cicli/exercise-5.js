c2b2f82e2549678fc9b4566d83828503c0df77f4
Roberta2017 <99208036+Roberta2017@users.noreply.github.com>
2022-04-11 09:02:44 +0200

    Add files via upload

---
+++ 08 - Blocchi condizionali e cicli/exercise-5.js
@@ -0,0 +1,32 @@
+function calculateAverageAge(persons) {
+  for(let i=0; i<persons.length; i++){
+    totale=totale+(persons[i].age);
+
+  }
+
+  totale=totale/persons.length;
+  return totale
+
+  }
+
+
+let totale=0;
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const averageAge = calculateAverageAge(persons);
+console.log(persons);
+console.log(averageAge);
+
+
+
