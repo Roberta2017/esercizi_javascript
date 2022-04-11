d94e8c5d6bdc61d6bdada483ecaec278b255f818
Roberta2017 <99208036+Roberta2017@users.noreply.github.com>
2022-04-11 17:26:21 +0200

    Add files via upload

---
+++ 16 - Closures/exercise-3.js
@@ -0,0 +1,20 @@
+function createStore() {
+ let shop =[];
+ return function(prodotto){
+   shop.push(prodotto)
+   console.log(shop)
+ }
+
+}
+const redPants = { id: 1, name: 'Red Pants' };
+const whiteHat = { id: 2, name: 'White Hat' };
+const blackSneakers = { id: 3, name: 'Black Sneakers' };
+
+console.log('--- Dress Store ---');
+const dressStore = createStore();
+dressStore(redPants);
+dressStore(whiteHat)
+
+console.log('--- Shoes Store ---');
+const shoesStore = createStore();
+shoesStore(blackSneakers);
