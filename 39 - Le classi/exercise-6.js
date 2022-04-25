5d655eee860891ceb2be35dbebc5b641a9e44e28
Roberta_undefined <99208036+Roberta2017@users.noreply.github.com>
2022-04-25 17:56:31 +0200

    Add files via upload

---
+++ 39 - Le classi/exercise-6.js
@@ -0,0 +1,49 @@
+class Square {
+  constructor(side) {
+    this.side = side;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+}
+
+class AreaCalculator {
+static calculate(param){ //se il parametro inserito  è uguale a side (in questo caso side corrisponde a 4) allora moltiplica il parametro inserito (side,4)
+      // per se stesso (latoxlato)
+  if(param.side){
+    return param.side*param.side;
+  }
+
+  else if(param.width && param.height){ //se invece viene rilevato che param è = a width e height allora moltiplicali fra loro
+    return param.width*param.height;
+  }
+
+  else if (param.radius){//se viene rilevato un param=a radius fai blablabla
+    return param.radius * param.radius * 3.14;
+  }
+
+else{
+  return "o jolly"
+}
+
+}
+
+}
+
+const square = new Square(4);
+const rectangle = new Rectangle(4, 2);
+const circle = new Circle(5);
+
+console.log(AreaCalculator.calculate(square));
+console.log(AreaCalculator.calculate(rectangle));
+console.log(AreaCalculator.calculate(circle));
---
+++ 39 - Le classi/ro.js
@@ -0,0 +1,54 @@
+class Figure {
+    constructor(area){
+      this.area = area;
+    }
+   }
+
+   class Square extends Figure {
+     constructor(side,area) {
+       super(area);
+       this.side = side;
+     }
+   }
+
+   class Rectangle extends Figure {
+     constructor(width, height, area) {
+       super(area);
+       this.width = width;
+       this.height = height;
+     }
+   }
+
+   class Circle extends Figure {
+     constructor(radius, area) {
+       super(area);
+       this.radius = radius;
+     }
+   }
+
+   class AreaCalculator {
+     static calculate(figure) {
+       let area;
+       if(figure instanceof Square){
+         area = (figure.side * figure.side);
+       }
+       else if(figure instanceof Rectangle){
+         area = (figure.width * figure.height);
+       }
+       else if (figure instanceof Circle){
+         area = (figure.radius * figure.radius * Math.PI);
+       }
+       else {
+         console.log("Figura non disponibile");
+       }
+       return(area);
+     }
+   }
+
+   const square = new Square(4);
+   const rectangle = new Rectangle(4, 2);
+   const circle = new Circle(5);
+
+   console.log(AreaCalculator.calculate(square));
+   console.log(AreaCalculator.calculate(rectangle));
+   console.log(AreaCalculator.calculate(circle));
