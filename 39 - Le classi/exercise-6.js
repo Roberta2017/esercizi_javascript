51cc214beb96b78b0cde3d7ddab16dfc886626a3
Roberta_undefined <99208036+Roberta2017@users.noreply.github.com>
2022-05-02 09:20:18 +0200

    Add files via upload

--- 39 - Le classi/exercise-6.js
+++ 39 - Le classi/exercise-6.js
@@ -18,25 +18,19 @@ class Circle {
 }
 
 class AreaCalculator {
-static calculate(param){ //se il parametro inserito  è uguale a side (in questo caso side corrisponde a 4) allora moltiplica il parametro inserito (side,4)
-      // per se stesso (latoxlato)
-  if(param.side){
-    return param.side*param.side;
+  static calculate(param) { //se il parametro inserito  è uguale a side (in questo caso side corrisponde a 4) allora moltiplica il parametro inserito (side,4)
+    // per se stesso (latoxlato)
+    if (param.side) {
+      return param.side * param.side;
+    } else if (param.width && param.height) { //se invece viene rilevato che param è = a width e height allora moltiplicali fra loro
+      return param.width * param.height;
+    } else if (param.radius) { //se viene rilevato un param=a radius fai blablabla
+      return param.radius * param.radius * 3.14;
+    } else {
+      return "o jolly"
+    }
+
   }
-
-  else if(param.width && param.height){ //se invece viene rilevato che param è = a width e height allora moltiplicali fra loro
-    return param.width*param.height;
-  }
-
-  else if (param.radius){//se viene rilevato un param=a radius fai blablabla
-    return param.radius * param.radius * 3.14;
-  }
-
-else{
-  return "o jolly"
-}
-
-}
 
 }
 
